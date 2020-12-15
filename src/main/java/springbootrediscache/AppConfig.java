c93d4e4bcbcee44515ef5dceae91cdd389a39c94
virus <lvlvlviruslslsl@gmail.com>
2020-12-15 23:22:46 +0200

    test_redis_2

--- src/main/java/springbootrediscache/AppConfig.java
+++ src/main/java/springbootrediscache/AppConfig.java
@@ -1,23 +1,13 @@
 package springbootrediscache;
 
 import javax.sql.DataSource;
-
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import springbootrediscache.models.User;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 @Configuration
 public class AppConfig {
 
@@ -33,4 +23,6 @@ public class AppConfig {
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
         return redisTemplate;
     }
+    //@Bean
+   // public DriverManagerDataSource dataSource() { /*code*/ }
 }
