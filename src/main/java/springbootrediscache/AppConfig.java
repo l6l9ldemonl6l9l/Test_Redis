826f75e3dc95fcee8333bae84e154ad5b56f6603
virus <lvlvlviruslslsl@gmail.com>
2020-12-16 09:26:14 +0200

    test_redis_4

--- src/main/java/springbootrediscache/AppConfig.java
+++ src/main/java/springbootrediscache/AppConfig.java
@@ -1,8 +1,13 @@
 package springbootrediscache;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,6 +16,16 @@ import springbootrediscache.models.User;
 
 @Configuration
 public class AppConfig {
+    @Resource
+    private Environment environment;
+    @Value("${spring.datasource.username}")
+    private String datasourceUsernameKey;
+
+    @Value("${spring.datasource.password}")
+    private String datasourcePasswordKey;
+
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
@@ -28,9 +43,18 @@ public class AppConfig {
     public DriverManagerDataSource getDataSource() {
         final DriverManagerDataSource  ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl(datasourceUsernameKey);
+        ds.setUsername(datasourcePasswordKey);
+        ds.setPassword(datasourceUrl);
+        return ds;
+    }
+  /**  @Bean
+    public DriverManagerDataSource getDataSource() {
+        final DriverManagerDataSource  ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/test");
         ds.setUsername("root");
         ds.setPassword("password");
         return ds;
-    }
+    }*/
 }
